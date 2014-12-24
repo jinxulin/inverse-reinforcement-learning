9a956105c07efc4c7b21e4bd4ea425d51be4bef4
Kintoki <jinxulin@liulishuo.com>
2014-12-24 15:40:30 +0800

    fix code

--- gui/gui_classes.py
+++ gui/gui_classes.py
@@ -3,35 +3,36 @@ from numpy import array
 
 SCREEN_WIDTH = 185
 SCREEN_HEIGHT = 660
+GUI_SPEED_MULTIPLE = 2
 background = pygame.image.load('resources/image/map.png')
 
 
 #create class Player
 class Player(pygame.sprite.Sprite):
-    def __init__(self, init_pos):
+    def __init__(self, init_pos, ini_speed):
         player_img = pygame.image.load('resources/image/car.png')
         player_rect = pygame.Rect(0, 0, 29, 45)
         self.image = player_img.subsurface(player_rect)
         self.rect = init_pos
-        self.speed = 60
-        self.speedGap = 60
+        self.speed = ini_speed
+        self.speed_horizontal = 60
         self.is_hit = False
 
     def speed_up(self):
-        if self.speedGap > 0:
-            self.speedGap -= 60
+        if self.speed < 150:
+            self.speed += 30
 
     def slow_down(self):
-        if self.speedGap < 180:
-            self.speedGap += 60
+        if self.speed > 30:
+            self.speed -= 30
 
     def move_left(self):
-        if self.rect[0] > self.speed:
-            self.rect[0] -= self.speed
+        if self.rect[0] > self.speed_horizontal:
+            self.rect[0] -= self.speed_horizontal
 
     def move_right(self):
-        if self.rect[0] < SCREEN_WIDTH-self.speed:
-            self.rect[0] += self.speed
+        if self.rect[0] < SCREEN_WIDTH-self.speed_horizontal:
+            self.rect[0] += self.speed_horizontal
 
 
 #create class Enemy
@@ -51,7 +52,7 @@ class Enemy(pygame.sprite.Sprite):
         self.speed = new_speed
 
     def move(self, seconds):
-        self.pos += self.speed*seconds
+        self.pos += self.speed * seconds * GUI_SPEED_MULTIPLE
         self.rect.top = self.pos
 
 
--- run_export_demo.py
+++ run_export_demo.py
@@ -1,25 +1,34 @@
+#encoding:utf-8
+
+"""
+run the experiment environment
+control the player's car by keyboard (up, down, speed_up, speed_down)
+record state and export's action at that state
+"""
+
 import random
+from numpy import array
 from gui.gui_classes import *
 from pygame.locals import *
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('car simulation')
+pygame.display.set_caption("car simulation: get export's data")
 
 # define the position of lane, bias, speed of player, speed of every lane
 line_dist = 13
-pos_list = array([0, 60, 120])+line_dist  # position
-player_speed = 300
-speed_list = array([200, 160, 100])
+pos_list = line_dist + array([0, 60, 120])  # position
+player_speed = 120
+speed_list = array([90, 60, 30])
 speed_list = player_speed - speed_list  # speed gap
 
 # init player and enemies
 player_pos = [120 + line_dist, 480]
-player = Player(player_pos)
+player = Player(player_pos, player_speed)
 enemies = pygame.sprite.Group()
 
 # other parameters
-state = array([2, 8, 8, 8])
+state = array([2, 8, 8, 8, player.speed])
 clock = pygame.time.Clock()
 generate_time = 0  # time to build a new car
 Time = 1000
@@ -39,7 +48,7 @@ while True:
     # generate the car randomly
     if generate_time >= Time:
         for i in range(3):
-            if len(enemies) == 0 or random.randint(0, 50) >= (45 - i*5):
+            if len(enemies) == 0 or random.randint(0, 50) >= (45 - i * 5):
                 enemy_pos = [pos_list[i], 0]
                 enemy = Enemy(enemy_pos, speed_list[i])
                 enemies.add(enemy)
@@ -47,15 +56,26 @@ while True:
 
     # move the enemy
     for enemy in enemies:
-        enemy.set_speed(enemy.speed - player.speedGap)  # update the speed
+        #enemy.set_speed(enemy.speed - player.speed)  # update the speed
         enemy.move(time_passed_seconds)
         if enemy.rect.top < 0:
             enemies.remove(enemy)
-        enemy.set_speed(enemy.speed + player.speedGap)
+        #enemy.set_speed(enemy.speed + player.speed)
     enemies.draw(screen)
 
     # update the screen
     pygame.display.update()
+
+    #get the state of three lane
+    state = array([2, 8, 8, 8, player.speed/30])
+    for enemy in enemies:
+        if (player.rect[1] - enemy.rect[1]) < -60:
+            continue
+        idx_enemy = list(pos_list).index(enemy.rect[0]) + 1
+        state[idx_enemy] = min([int(abs(player.rect[1] - enemy.rect[1])/60), state[idx_enemy]])
+    #get the current lane
+    state[0] = list(pos_list).index(player.rect[0])
+    print state
 
     #keybroad control
     if not key_push:
@@ -76,6 +96,21 @@ while True:
         pressed_keys = pygame.key.get_pressed()
         if not (pressed_keys[K_LEFT] or pressed_keys[K_RIGHT] or pressed_keys[K_UP] or pressed_keys[K_DOWN]):
             key_push = False
+
+    '''
+    #------------------record-----------------------#
+    if newStateIndex != oldStateIndex:
+        exportRecords.append([oldStateIndex, getAction( newStateIndex, oldStateIndex)])
+        oldStateIndex = newStateIndex
+        writeNum -= 1
+    if writeNum<=0:
+        f = open("data/export.txt","a")
+        for line in exportRecords:
+            f.write(str(line[0])+"\t"+str(line[1])+"\n")
+        f.close()
+        writeNum=120
+        print "written"
+    '''
 
     # exit the game
     for event in pygame.event.get():
