ca791c37c4484133d743b595d8e1deeadecd016d
Kintoki <jinxulin@liulishuo.com>
2015-01-21 10:10:13 +0800

    fixed

--- mdp_model_test.py
+++ mdp_model_test.py
@@ -1,11 +1,13 @@
 from driving.model import *
 from driving.reward import DrivingReward
 from mdp.solver import QLearningSolver
-import numpy as np
+from numpy import array
 from gui.run_gui import *
 
 reward = DrivingReward()
-reward.params = np.array([0.05, 0.02, 0.01, 0.2, 0.5, 0.6, 0.7, 0.8, -3, -0.05, -0.01])
+reward.params = array([0.05, 0.02, 0.01, 0.2, 0.5, 0.6, 0.7, 0.8, -3, -0.05, -0.01])
+#reward.params = array([0.15680481, 0.21396384, 0.00576546, 0.05409643, -0.01852762, 0.03494856,
+#                       0.01832426, 0.14674493, -0.21742997, -0.04897249, 0.20452204])
 model = DrivingModel()
 model.reward_function = reward
 model.dim = 11
